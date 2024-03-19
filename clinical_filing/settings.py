d0bdfb310b76a78027249fdc7d0c2ab6e982d87f
Josinimasango <133340361+Josinimasango@users.noreply.github.com>
2024-03-19 20:54:57 +0200

    modified

--- clinical_filing/settings.py
+++ clinical_filing/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
     'default': {
        # 'ENGINE': 'django.db.backends.sqlite3',
        # 'NAME': BASE_DIR / 'db.sqlite3',
-       'ENGINE':'django.db.backends.postgresql_psycopg2',
+       'ENGINE':'django.db.backends.postgresql',
        'NAME':'clinical',
        'USER':'postgres',
        'PASSWORD':'Nelisiwe@94',
@@ -126,8 +126,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 if DEBUG:
     STATICTFILES_DIRS = [BASE_DIR / "static"]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- clinical_filing/wsgi.py
+++ clinical_filing/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'clinical_filing.settings')
 
 application = get_wsgi_application()
+
+application = application
